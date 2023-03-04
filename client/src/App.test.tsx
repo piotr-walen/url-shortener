07bdeb12f93563cec082782c0201ff2073d4792a
Piotr Walen <walen.piotr@gmail.com>
2023-03-04 18:47:34 +0100

    Refactor test using userEvent lib

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,13 +1,11 @@
-// import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
-
+import { describe, expect, it, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
 import { shorten, ShortenStatus } from "./api/shorten";
 
 vi.mock("./api/shorten", () => ({ shorten: vi.fn() }));
-
 
 
 describe("App", () => {
@@ -18,21 +16,20 @@ describe("App", () => {
     expect(screen.getByText('Namespace is required')).toBeDefined();
   });
 
-  it('should allow to enter namespace', () => {
+  it('should allow to enter namespace', async () => {
+    userEvent.setup();
     render(<App />);
     const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    
-    fireEvent.change(namespaceInput, { target: { value: "test" } });
-   
-    expect(namespaceInput.value).toBe("test");
+
+    await userEvent.type(namespaceInput, "test")
+
+    expect(namespaceInput).toHaveValue("test")
   });
 
-  it('should allow to proceed to next step when valid namespace is entered', () => {
+  it('should allow to proceed to next step when valid namespace is entered', async () => {
     render(<App />);
-    const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    fireEvent.change(namespaceInput, { target: { value: "test" } });
-    
-    fireEvent.click(screen.getByText('Next'));
+    await userEvent.type(screen.getByLabelText('Namespace'), "test");
+    await userEvent.click(screen.getByText('Next'));
     
     expect(screen.getByText('Enter URL that you would like to create alias for')).toBeDefined();
     expect(screen.getByText('Target URL is required')).toBeDefined();
@@ -43,82 +40,75 @@ describe("App", () => {
     vi.mocked(shorten).mockImplementationOnce(async () => ({ status: 'created' as ShortenStatus }))
 
     render(<App />);
-    const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    fireEvent.change(namespaceInput, { target: { value: "test" } });
-    fireEvent.click(screen.getByText('Next'));
-    const urlInput: HTMLInputElement = screen.getByLabelText('Target URL');
-    const segmentInput: HTMLInputElement = screen.getByLabelText('Alias');
-    fireEvent.change(urlInput, { target: { value: "https://vitest.dev" } });
-    fireEvent.change(segmentInput, { target: { value: "vitest" } });
-    
-    fireEvent.click(screen.getByText('Next'));
+    await userEvent.type(screen.getByLabelText('Namespace'), "test");
+    await userEvent.click(screen.getByText('Next'));
+    await userEvent.type(screen.getByLabelText('Target URL'), "https://vitest.dev");
+    await userEvent.type(screen.getByLabelText('Alias'), "vitest");
+
+    await userEvent.click(screen.getByText('Next'))
     
     await waitFor(() => expect(screen.getByText("Here's your new url")).toBeDefined())
     await waitFor(() => expect(screen.getByText("http://localhost:8000/test/vitest")).toBeDefined())
   });
 
-  it('should render error when namespace invalid', () => {
+  it('should render error when namespace invalid', async () => {
     render(<App />);
     const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    
-    fireEvent.change(namespaceInput, { target: { value: "//" } });
-    
-    expect(namespaceInput.value).toBe("//");
+
+    await userEvent.type(namespaceInput, "//");
+
+    expect(namespaceInput).toHaveValue("//")
     expect(screen.getByText('Namespace field can only contain alphanumeric or "-", "_" characters')).toBeDefined();
   });
 
-  it('should render error when invalid url or alias is entered', () => {
+  it('should render error when invalid url or alias is entered', async () => {
     render(<App />);
-    const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    fireEvent.change(namespaceInput, { target: { value: "test" } });
-    fireEvent.click(screen.getByText('Next'));
-    const urlInput: HTMLInputElement = screen.getByLabelText('Target URL');
-    const aliasInput: HTMLInputElement = screen.getByLabelText('Alias');
-    
-    fireEvent.change(urlInput, { target: { value: "//" } });
-    fireEvent.change(aliasInput, { target: { value: "$#@" } });
+    await userEvent.type(screen.getByLabelText('Namespace'), "test");
+    await userEvent.click(screen.getByText('Next'));
+
+    await userEvent.type(screen.getByLabelText('Target URL'), "//") ;
+    await userEvent.type(screen.getByLabelText('Alias'), "$#@");
     
     expect(screen.getByText('Target URL should contain valid http or https URL')).toBeDefined();
     expect(screen.getByText('Alias field can only contain alphanumeric or "-", "_" characters')).toBeDefined();
   });
 
-  it('should not allow to proceed to second step when invalid namespace is entered', () => {
+  it('should not allow to proceed to second step when invalid namespace is entered', async () => {
     render(<App />);
-    const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    fireEvent.change(namespaceInput, { target: { value: "//" } });
+    await userEvent.type(screen.getByLabelText('Namespace'), "//");
    
-    fireEvent.click(screen.getByText('Next'));
+    await userEvent.click(screen.getByText('Next'));
     
     expect(screen.getByText(`"Log in" to your namespace`)).toBeDefined();
   });
 
-  it('should not allow to proceed to third step when invalid alias or target url is entered', () => {
+  it('should not allow to proceed to third step when invalid alias or target url is entered', async () => {
     render(<App />);
-    const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    fireEvent.change(namespaceInput, { target: { value: "test" } });
-    fireEvent.click(screen.getByText('Next'));
-    const urlInput: HTMLInputElement = screen.getByLabelText('Target URL');
-    fireEvent.change(urlInput, { target: { value: "" } });
+    await userEvent.type(screen.getByLabelText('Namespace'), "test");
+    
+    await userEvent.click(screen.getByText('Next'));
+    
+    await userEvent.type(screen.getByLabelText('Target URL'), "a");
+    await userEvent.clear(screen.getByLabelText('Target URL'));
 
-    fireEvent.click(screen.getByText('Next'));
+    await userEvent.click(screen.getByText('Next'));
 
     expect(screen.getByText('Target URL is required')).toBeDefined();
   });
+
 
   it('should render generic error when server responds with error', async () => {
     vi.mocked(shorten).mockImplementationOnce(async () => Promise.resolve({ status: 'serverError' as ShortenStatus }))
 
     render(<App />);
 
-    const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    fireEvent.change(namespaceInput, { target: { value: "test" } });
-    fireEvent.click(screen.getByText('Next'));
-    const urlInput: HTMLInputElement = screen.getByLabelText('Target URL');
-    const segmentInput: HTMLInputElement = screen.getByLabelText('Alias');
-    fireEvent.change(urlInput, { target: { value: "https://vitest.dev" } });
-    fireEvent.change(segmentInput, { target: { value: "vitest" } });
+    await userEvent.type(screen.getByLabelText('Namespace'), "test");
+    await userEvent.click(screen.getByText('Next'));
+
+    await userEvent.type(screen.getByLabelText('Target URL'), "https://vitest.dev");
+    await userEvent.type(screen.getByLabelText('Alias'),  "vitest" );
     
-    fireEvent.click(screen.getByText('Next'));
+    await userEvent.click(screen.getByText('Next'));
 
     await waitFor(() => expect(screen.getByText("Internal error.", { exact: false })).toBeDefined())
   })
@@ -128,15 +118,12 @@ describe("App", () => {
 
     render(<App />);
 
-    const namespaceInput: HTMLInputElement = screen.getByLabelText('Namespace');
-    fireEvent.change(namespaceInput, { target: { value: "test" } });
-    fireEvent.click(screen.getByText('Next'));
-    const urlInput: HTMLInputElement = screen.getByLabelText('Target URL');
-    const segmentInput: HTMLInputElement = screen.getByLabelText('Alias');
-    fireEvent.change(urlInput, { target: { value: "https://vitest.dev" } });
-    fireEvent.change(segmentInput, { target: { value: "vitest" } });
+    await userEvent.type(screen.getByLabelText('Namespace'), "test");
+    await userEvent.click(screen.getByText('Next'));
+    await userEvent.type(screen.getByLabelText('Target URL'), "https://vitest.dev");
+    await userEvent.type(screen.getByLabelText('Alias'), "vitest");
     
-    fireEvent.click(screen.getByText('Next'));
+    await userEvent.click(screen.getByText('Next'));
 
     await waitFor(() => expect(screen.getByText("Url is already taken. Please enter another alias.")).toBeDefined())
   })
